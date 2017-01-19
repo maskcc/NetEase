fa5cf0485f6c8c190faa43cedfddbb2cc04f2c39
pickmio <mioguyu@gmail.com>
2017-01-19 11:13:50 +0800

    1. push thirds

---
+++ thirds.sh
@@ -0,0 +1,10 @@
+mkdir ../third
+cd ../third
+#1. glog
+cd ../third;git clone https://github.com/google/glog.git
+#2. libevents 
+cd ../third;git clone https://github.com/nmathewson/Libevent.git
+#3. protobuf
+cd ../third;git clone https://github.com/google/protobuf.git
+#4. rapidjson
+cd ../third;git clone https://github.com/miloyip/rapidjson.git
