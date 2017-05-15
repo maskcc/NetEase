c4eacf07274a2d147ba44c639b92e0c13355de67
pickmio <mioguyu@gmail.com>
2017-05-15 17:00:04 +0800

    1. protocols

--- proto/make.sh
+++ proto/make.sh
@@ -1,8 +1,12 @@
 #用makefile 来编写, 要编译与清除dest目录的内容
-mkdir -p protocols
 SRC_DIR=./src
 DST_DIR=./protocols
+CSP_DIR=./csharp_proto
+mkdir -p ${DST_DIR}
+mkdir -p ${CSP_DIR}
+
 protoc  -I=$SRC_DIR --cpp_out=$DST_DIR $SRC_DIR/book.proto
+protoc  -I=$SRC_DIR --csharp_out=$CSP_DIR $SRC_DIR/book.proto
 
 
 
